0e45f814ddb6b251eca7951d3b96ae63454541ce
vfa-datct <datct@vitalify.asia>
2022-05-25 00:37:42 +0700

    add logic to tables

--- src/Components/Layout/Dashboard/Dashboard.tsx
+++ src/Components/Layout/Dashboard/Dashboard.tsx
@@ -21,6 +21,8 @@ import { Line } from 'react-chartjs-2';
 import './dashboard.scss';
 import StatsPanel from './StatsPanel/StatsPanel';
 import Arrow from '../../../Assets/Arrow';
+import Select from '../../Select/Select';
+const selectOptions = ['Ngày', 'Tháng', 'Năm'];
 const Counter: React.FC = () => {
   ChartJS.register(
     CategoryScale,
@@ -59,91 +61,93 @@ const Counter: React.FC = () => {
   };
   return (
     <div className="app__layout-dashboard__counter">
-      <div className="counter-label">Biểu đồ cấp số</div>
-      <div className="counter-cards">
-        <div className="counter-cards__card">
-          <div className="row">
-            <div className="image blue">
-              <Calendar1 />
+      <div className="app__layout-dashboard__counter__container">
+        <div className="counter-label">Biểu đồ cấp số</div>
+        <div className="counter-cards">
+          <div className="counter-cards__card">
+            <div className="row">
+              <div className="image blue">
+                <Calendar1 />
+              </div>
+              <div className="label">Số thứ tự đã cấp</div>
             </div>
-            <div className="label">Số thứ tự đã cấp</div>
-          </div>
-          <div className="row">
-            <div className="count">4.221</div>
-            <div className="diff raise">
-              <Arrow />
-              32,41%
+            <div className="row">
+              <div className="count">4.221</div>
+              <div className="diff raise">
+                <Arrow />
+                32,41%
+              </div>
             </div>
           </div>
-        </div>
-        <div className="counter-cards__card">
-          <div className="row">
-            <div className="image green">
-              <Calendar2 />
+          <div className="counter-cards__card">
+            <div className="row">
+              <div className="image green">
+                <Calendar2 />
+              </div>
+              <div className="label">Số thứ tự đã cấp</div>
             </div>
-            <div className="label">Số thứ tự đã cấp</div>
-          </div>
-          <div className="row">
-            <div className="count">3.721</div>
+            <div className="row">
+              <div className="count">3.721</div>
 
-            <div className="diff drop">
-              <Arrow />
-              32,41%
+              <div className="diff drop">
+                <Arrow />
+                32,41%
+              </div>
+            </div>
+          </div>
+          <div className="counter-cards__card">
+            <div className="row">
+              <div className="image orange">
+                <Wait />
+              </div>
+              <div className="label">Số thứ tự đã cấp</div>
+            </div>
+            <div className="row">
+              <div className="count">468</div>
+              <div className="diff drop">
+                <Arrow />
+                32,41%
+              </div>
+            </div>
+          </div>
+          <div className="counter-cards__card">
+            <div className="row">
+              <div className="image red">
+                <Passing />
+              </div>
+              <div className="label">Số thứ tự đã cấp</div>
+            </div>
+            <div className="row">
+              <div className="count">32</div>
+              <div className="diff raise">
+                <Arrow />
+                32,41%
+              </div>
             </div>
           </div>
         </div>
-        <div className="counter-cards__card">
+        <div className="counter-chart">
           <div className="row">
-            <div className="image orange">
-              <Wait />
+            <div className="col">
+              <div className="chart-label">Bảng thống kê theo ngày</div>
+              <div className="chart-month-of-year">Tháng 11/2021</div>
             </div>
-            <div className="label">Số thứ tự đã cấp</div>
-          </div>
-          <div className="row">
-            <div className="count">468</div>
-            <div className="diff drop">
-              <Arrow />
-              32,41%
+            <div className="col">
+              <div className="view-mode">
+                <span>Xem theo</span>
+                <Select options={selectOptions} onSelectedChange={change} />
+              </div>
             </div>
           </div>
+          <Line data={data} options={options} />
         </div>
-        <div className="counter-cards__card">
-          <div className="row">
-            <div className="image red">
-              <Passing />
-            </div>
-            <div className="label">Số thứ tự đã cấp</div>
-          </div>
-          <div className="row">
-            <div className="count">32</div>
-            <div className="diff raise">
-              <Arrow />
-              32,41%
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="counter-chart">
-        <div className="row">
-          <div className="col">
-            <div className="chart-label">Bảng thống kê theo ngày</div>
-            <div className="chart-month-of-year">Tháng 11/2021</div>
-          </div>
-          <div className="col">
-            <div className="view-mode">
-              <span>Xem theo</span>
-              <select className="view-mode__selector">
-                <option value="day">Ngày</option>
-                <option value="month">Tháng</option>
-                <option value="year">Năm</option>
-              </select>
-            </div>
-          </div>
-        </div>
-        <Line data={data} options={options} />
       </div>
     </div>
   );
+};
+
+const change = (newValue: string) => {
+  console.log(newValue);
 };
 
 const Dashboard: React.FC = () => {
