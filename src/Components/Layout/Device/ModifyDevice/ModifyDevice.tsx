2a390430d4718766f8e6d9993629d110b4213c68
vfa-datct <datct@vitalify.asia>
2022-05-25 14:27:52 +0700

    tach component

---
+++ src/Components/Layout/Device/ModifyDevice/ModifyDevice.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './modifydevice.scss';
+
+const ModifyDevice = () => {
+  return <div className="modify-device"></div>;
+};
+
+export default ModifyDevice;
---
+++ src/Components/Layout/Device/NewDevice/NewDevice.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './newdevice.scss';
+
+const NewDevice = () => {
+  return <div className="new-device"></div>;
+};
+
+export default NewDevice;
