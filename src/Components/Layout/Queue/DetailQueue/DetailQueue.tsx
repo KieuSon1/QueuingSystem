ca8e9a2bbe1d4e5c14b11300d59a6a0ef4d8265d
pdHHHH <huyducpham2000@gmail.com>
2022-05-28 16:24:27 +0700

    update detail device

--- src/Components/Layout/Queue/DetailQueue/DetailQueue.tsx
+++ src/Components/Layout/Queue/DetailQueue/DetailQueue.tsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import "./detailqueue.scss"
 import { useNavigate } from 'react-router';
+import { ReactComponent as ReturnIc } from '../../../../Assets/return.svg';
 
 const DetailQueue: React.FC = () => {
-   
+
     const navigate = useNavigate()
     return (
-        <div className='detail-queue'>
-            <div className="title">Quản lý cấp số</div>
-            <div className="detail-queue__container">
-                <div className='row form-label'>Thông tin thiết bị</div>
-                <div className="row"><div className='flex-1'>Mã thiết bị:</div><div className='flex-1'>Loại thiết bị:</div></div>
-                <div className="row"><div className='flex-1'>Tên thiết bị:</div><div className='flex-1'>Tên đăng nhập:</div></div>
-                <div className="row"><div className='flex-1'>Địa chỉ IP:</div><div className='flex-1'>Mật khẩu:</div></div>
-                <div className="row"><div className='flex-1'>Dịch vụ sử dụng:</div></div>
+        <div className='app__queue-layout__detail-queue'>
+            <div className='app__queue-layout__detail-queue__title'>Quản lý cấp số</div>
+            <div className='app__queue-layout__detail-queue__container'>
+                <div className='row form-label'>Thông tin cấp số</div>
+                <div className="row"><div className='flex-1'>Họ tên:</div><div className='flex-1'>Loại thiết bị:</div></div>
+                <div className="row"><div className='flex-1'>Tên dịch vụ:</div><div className='flex-1'>Trạng thái:</div></div>
+                <div className="row"><div className='flex-1'>Số thứ tự:</div><div className='flex-1'>Số điện thoại:</div></div>
+                <div className="row"><div className='flex-1'>Thời gian cấp:</div><div className='flex-1'>Địa chỉ Email:</div></div>
+                <div className="row"><div className='flex-1'>Hạn sử dụng:</div></div>
             </div>
-            
+            <div
+                className='app__queue-layout__detail-queue__float-btn'
+                onClick={() => navigate('/dashboard/queue/list')}
+            >
+                <span>
+                    <ReturnIc />
+                </span>
+                <span>Chỉnh sửa thiết bị</span>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default DetailQueue
